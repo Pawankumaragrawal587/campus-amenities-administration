d6dbaeea7f4d162b23e2d8dee76c27cdcb125930
Nkbdox10 <54908834+Nkbdox10@users.noreply.github.com>
2021-11-23 22:22:41 +0530

    Update market.js

--- routes/market.js
+++ routes/market.js
@@ -7,4 +7,23 @@ router.get('/market',function(req,res){
     res.render("market/index");
 });
 
+router.get('/market/shopBooking',function(req,res){
+    res.render("market/shopBooking");
+});
+
+router.get('/market/shopRentingRequests',function(req,res){
+    res.render("market/shopRentingRequests");
+});
+
+router.get('/market/billPaymentform',function(req,res){
+    res.render("market/billPaymentform");
+});
+
+router.get('/market/billPaymentRequests',function(req,res){
+    res.render("market/billPaymentRequests");
+});
+
+router.get('/market/feedbackform',function(req,res){
+    res.render("market/feedbackform");
+});
 module.exports = router;
